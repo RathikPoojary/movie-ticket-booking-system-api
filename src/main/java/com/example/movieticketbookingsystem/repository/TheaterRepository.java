fee6ad3ff95ee367aa35be987bdccf606175d45d
rathik78 <rathikpoojary78@gmail.com>
2025-04-25 12:33:41 +0530

    theater update done successfully

--- src/main/java/com/example/movieticketbookingsystem/repository/TheaterRepository.java
+++ src/main/java/com/example/movieticketbookingsystem/repository/TheaterRepository.java
@@ -4,6 +4,8 @@ package com.example.movieticketbookingsystem.repository;
 import com.example.movieticketbookingsystem.entity.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TheaterRepository extends JpaRepository<Theater,String> {
 
 }
