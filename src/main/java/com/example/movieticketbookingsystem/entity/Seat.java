f4d66970e573b1fe5ab7b06902cc3fa7750a2ae6
rathik78 <rathikpoojary78@gmail.com>
2025-04-29 22:48:45 +0530

    auditing  done

--- src/main/java/com/example/movieticketbookingsystem/entity/Seat.java
+++ src/main/java/com/example/movieticketbookingsystem/entity/Seat.java
@@ -5,21 +5,28 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Setter
 @Getter
 @ToString
+@Table(name = "seat")
 public class Seat {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "seat_id",nullable = false)
     private String seatId;
 
+    @Column(name = "created_at",updatable = false,nullable = false)
     @CreatedDate
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @ManyToOne
+    @JoinColumn(name = "screen_id")
     private Screen screen;
 }
--- src/main/java/com/example/movieticketbookingsystem/entity/UserDetails.java
+++ src/main/java/com/example/movieticketbookingsystem/entity/UserDetails.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,26 +19,50 @@ import java.time.LocalDate;
 @Getter
 @ToString
 @EntityListeners(AuditingEntityListener.class)
+@Table(name = "user_details")
 public class UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id",nullable = false)
     private String userId;
+
+    @Column(name = "username",nullable = false)
     private String username;
-    @Column(unique = true)
+
+    @Column(name = "email", unique = true,nullable = false)
     private String email;
+
+    @Column(name = "password",nullable = false)
     private String password;
+
+    @Column(name = "user_role",nullable = false)
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
+
+    @Column(name = "phone-number",updatable = true,length = 10,nullable = false)
     private String phoneNumber;
+
+    @Column(name = "date_of_birth",nullable = false)
     private LocalDate dateOfBirth;
+
+    @Column(name = "is_delete",nullable = false)
     private boolean isDelete;
+
+    @Column(name = "deleted_at",nullable = false)
     private Instant deletedAt;
 
+    @Column(name = "created_at",updatable = false,nullable = false)
     @CreatedDate
     private Instant createdAt;
+
+    @Column(name = "updated_at",nullable = false)
     @LastModifiedDate
     private Instant updatedAt;
+
+    @Column(name = "created_by",updatable = false)
+    @CreatedBy
+    private String createdBy;
 
 
 }
